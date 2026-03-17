4fc8b82439194825deeacaa898a6a4ec82ecfb72
Juan Paez <jpaez@pernine.com.co>
2026-03-16 23:59:53 -0500

    fix: corrige suscripciones sin liberacion

--- src/app/features/transactions/pages/transactions-page.component.ts
+++ src/app/features/transactions/pages/transactions-page.component.ts
@@ -1,32 +1,26 @@
-import { Component, OnInit } from '@angular/core'
-import { delay } from 'rxjs'
+import { Component } from '@angular/core'
+import { delay, map, startWith } from 'rxjs'
 import { TransactionsService } from 'src/app/core/services/transactions.service'
 import { TableColumn } from 'src/app/shared/models/data-table.model'
-import { Transaction } from 'src/app/shared/models/transaction.model'
 import { COLUMNS_TRANSACTIONS } from 'src/app/utils/constants'
 
 @Component({
     selector: 'app-transactions-page',
     templateUrl: './transactions-page.component.html'
 })
-export class TransactionsPageComponent implements OnInit {
-    transactions: Transaction[] = []
+export class TransactionsPageComponent {
+
     columns: TableColumn[] = COLUMNS_TRANSACTIONS
-    isLoading = true
+
+    transactions$ = this.transactionsService.transactions$.pipe(
+        delay(1000)
+    )
+
+    isLoading$ = this.transactions$.pipe(
+        map(() => false),
+        startWith(true)
+    )
 
     constructor(private transactionsService: TransactionsService) { }
-
-    ngOnInit() {
-        this.transactionsService.transactions$.pipe(delay(1000))
-            .subscribe({
-                next: transactions => {
-                    this.transactions = transactions
-                    this.isLoading = false
-                },
-                error: () => {
-                    this.isLoading = false
-                }
-            })
-    }
 
 }
